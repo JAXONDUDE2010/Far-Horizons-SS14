c650d173d4d9655876223f555aef521872adcaa4
princess-gurchi <kharitonovich.m+pg@gmail.com>
2025-10-01 20:07:39 +0300

    Add InteractRestriction component for prototypers

---
+++ Content.Shared/_FarHorizons/Util/SharedIteractRestrictionSystem.cs
@@ -0,0 +1,45 @@
+﻿using Content.Shared._FarHorizons.Util.Components;
+using Content.Shared.Tag;
+
+namespace Content.Shared._FarHorizons.Util;
+
+public sealed class InteractRestrictionSystem : EntitySystem
+{
+    [Dependency] private readonly TagSystem _tagSystem = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<InteractRestrictionComponent, CheckItemCanBeUsedEvent>(CheckItemCanBeUsed);
+    }
+
+    private void CheckItemCanBeUsed(Entity<InteractRestrictionComponent> ent, ref CheckItemCanBeUsedEvent ev)
+    {
+        if (ev.Cancelled)
+            return;
+
+        var target = ev.Target;
+        var user = ev.User;
+
+        if (target != null && ent.Comp.RestrictInteractionTarget is InteractRestrictionList targetRestrict) {
+            if (targetRestrict.Blacklist != null &&
+                _tagSystem.HasAnyTag(target.Value, targetRestrict.Blacklist))
+                    ev.Cancel();
+            
+            if (targetRestrict.Whitelist != null &&
+                !_tagSystem.HasAnyTag(target.Value, targetRestrict.Whitelist))
+                    ev.Cancel();
+        }
+
+        if (ent.Comp.RestrictInteractionSource is InteractRestrictionList sourceRestrict) {
+            if (sourceRestrict.Blacklist != null &&
+                _tagSystem.HasAnyTag(user, sourceRestrict.Blacklist))
+                    ev.Cancel();
+            
+            if (sourceRestrict.Whitelist != null &&
+                !_tagSystem.HasAnyTag(user, sourceRestrict.Whitelist))
+                    ev.Cancel();
+        }
+    }
+}
