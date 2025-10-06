ac6211a2b05c74c7fb7e74832c7637de09f34682
princess-gurchi <kharitonovich.m+pg@gmail.com>
2025-10-06 16:24:42 +0300

    Don't fail nukie test when one of nukies happens to be shadekin

--- Content.IntegrationTests/Tests/GameRules/NukeOpsTest.cs
+++ Content.IntegrationTests/Tests/GameRules/NukeOpsTest.cs
@@ -229,12 +229,14 @@ public sealed class NukeOpsTest
         var totalSeconds = 30;
         var totalTicks = (int) Math.Ceiling(totalSeconds / server.Timing.TickPeriod.TotalSeconds);
         var increment = 5;
-        var resp = entMan.GetComponent<RespiratorComponent>(player);
+        entMan.TryGetComponent<RespiratorComponent>(player, out var resp); // Far Horizons change, not everyone breathes
         var damage = entMan.GetComponent<DamageableComponent>(player);
         for (var tick = 0; tick < totalTicks; tick += increment)
         {
             await pair.RunTicksSync(increment);
-            Assert.That(resp.SuffocationCycles, Is.LessThanOrEqualTo(resp.SuffocationCycleThreshold));
+            // Far Horizons change, not everyone breathes
+            if (resp != null)
+                Assert.That(resp.SuffocationCycles, Is.LessThanOrEqualTo(resp.SuffocationCycleThreshold));
             Assert.That(damage.TotalDamage, Is.EqualTo(FixedPoint2.Zero));
         }
 
