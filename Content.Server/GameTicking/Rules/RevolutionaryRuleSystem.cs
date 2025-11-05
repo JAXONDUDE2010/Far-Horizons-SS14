5db2c143353017b583f77c1c37567037b3b2b7db
jhrushbe <capnmerry@gmail.com>
2025-11-05 00:03:47 -0600

    Undo random edit VS made

--- Content.Server/GameTicking/Rules/RevolutionaryRuleSystem.cs
+++ Content.Server/GameTicking/Rules/RevolutionaryRuleSystem.cs
@@ -187,7 +187,7 @@ public sealed class RevolutionaryRuleSystem : GameRuleSystem<RevolutionaryRuleCo
                             _specialLobbyContent.SetSpecialLobbyContent(uid);
                             
                             // End the round
-                            // _audio.PlayGlobal("/Audio/_Starlight/Misc/sov_win.ogg", filter, false);
+                            // _audioSystem.PlayGlobal("/Audio/_Starlight/Misc/sov_win.ogg", filter, false);
                             _roundEnd.EndRound();
                         });
                     }
