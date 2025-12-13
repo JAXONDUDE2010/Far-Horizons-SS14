da402ed30d28f97fd1ec8b08e8f7fba0fd07b0b9
princess-gurchi <kharitonovich.m+pg@gmail.com>
2025-12-13 16:18:33 +0200

    resolve viewport stutter when opening UI

--- Content.Client/_FarHorizons/Research/UI/FHResearchTree.xaml.cs
+++ Content.Client/_FarHorizons/Research/UI/FHResearchTree.xaml.cs
@@ -24,6 +24,7 @@ public sealed partial class FHResearchTree : BoxContainer
     public Action<ProtoId<ResearchTreeNodePrototype>?>? OnSelectionChanged;
 
     private bool _moving = false;
+    private bool _viewportInit = false;
     private Vector2 _viewportPosition = Vector2.Zero;
     private Vector2 _pseudoViewport => (_viewportPosition * _zoom) + (_viewportSize / 2);
     private Vector2 _lastViewportPosition = Vector2.Zero;
@@ -100,6 +101,8 @@ public sealed partial class FHResearchTree : BoxContainer
         CanKeyboardFocus = true;
         DefaultCursorShape = CursorShape.Arrow;
         _search = new(_timing, _prototypeManager, _font, _searchTexture);
+
+        _viewportInit = false;
     }
     
     public void BuildTree(
@@ -120,8 +123,6 @@ public sealed partial class FHResearchTree : BoxContainer
         _researched = researched;
         _researching = progress;
         _queuedNodes = queued;
-
-        _viewportPosition = (-_viewportSize / 2) + new Vector2(NodeMarginHorizontal * 2, NodeMarginVertical * 2);
 
         _searchDb.Build(_prototypeManager, [.. nodes.Select(p => (ProtoId<ResearchTreeNodePrototype>)p.ID)]);
         _search.SetDb(_searchDb);
@@ -237,6 +238,12 @@ public sealed partial class FHResearchTree : BoxContainer
     protected override void Draw(DrawingHandleScreen handle)
     {
         base.Draw(handle);
+
+        if (!_viewportInit)
+        {
+            _viewportPosition = (-_viewportSize / 2) + new Vector2(NodeMarginHorizontal * 2, NodeMarginVertical * 2);
+            _viewportInit = true;
+        }
 
         if (!_draw)
             return;
