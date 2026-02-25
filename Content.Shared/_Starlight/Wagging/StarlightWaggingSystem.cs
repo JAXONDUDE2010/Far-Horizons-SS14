0f791bae9f78fa63bbfeb6bb6100d9c6750428b5
princess-gurchi <kharitonovich.m+pg@gmail.com>
2026-02-25 21:43:27 +0200

    fix wagging

--- Content.Shared/_Starlight/Wagging/StarlightWaggingSystem.cs
+++ Content.Shared/_Starlight/Wagging/StarlightWaggingSystem.cs
@@ -1,7 +1,6 @@
 ﻿using Content.Shared._Starlight.Humanoid.Markings;
 using Content.Shared.Actions;
-using Content.Shared.Humanoid;
-using Content.Shared.Humanoid.Markings;
+using Content.Shared.Body;
 using Content.Shared.Wagging;
 
 namespace Content.Shared._Starlight.Wagging;
@@ -10,34 +9,42 @@ public sealed class StarlightWaggingSystem : EntitySystem
 {
     [Dependency] private readonly SharedActionsSystem _actions = default!;
     [Dependency] private readonly StarlightMarkingSystem _starlightMarking = default!;
+    [Dependency] private readonly SharedVisualBodySystem _visualBody = default!;
 
     public override void Initialize()
     {
-        //SubscribeLocalEvent<WaggingComponent, MapInitEvent>(OnMapInit);
-        //SubscribeLocalEvent<WaggingComponent, MarkingsUpdateEvent>(OnMarkingsUpdate);
+        SubscribeLocalEvent<WaggingComponent, MapInitEvent>(OnMapInit);
+        SubscribeLocalEvent<WaggingComponent, ApplyOrganMarkingsEvent>(OnMarkingsUpdate);
     }
 
-    // private void OnMapInit(Entity<WaggingComponent> ent, ref MapInitEvent args) => UpdateAction(ent);
+    private void OnMapInit(Entity<WaggingComponent> ent, ref MapInitEvent args) => UpdateAction(ent);
 
-    // private void OnMarkingsUpdate(Entity<WaggingComponent> ent, ref MarkingsUpdateEvent args)
-    // {
-    //     // SpawnRandomHumanoid creates uninitialized entities and raises this before init
-    //     if (!ent.Comp.Running)
-    //         return;
+    private void OnMarkingsUpdate(Entity<WaggingComponent> ent, ref ApplyOrganMarkingsEvent args)
+    {
+        // SpawnRandomHumanoid creates uninitialized entities and raises this before init
+        if (!ent.Comp.Running)
+            return;
 
-    //     UpdateAction(ent);
-    // }
+        UpdateAction(ent);
+    }
 
-    // private void UpdateAction(Entity<WaggingComponent> ent)
-    // {
-    //     if (!TryComp<HumanoidAppearanceComponent>(ent, out var humanoid)) return;
-    //     if (!humanoid.MarkingSet.Markings.TryGetValue(MarkingCategories.Tail, out var markings)) return;
+    private void UpdateAction(Entity<WaggingComponent> ent)
+    {
+        if (!TryComp<VisualBodyComponent>(ent, out var body)) return;
+        if (!_visualBody.TryGatherMarkingsData((ent.Owner, body),
+                [ent.Comp.Layer],
+                out _,
+                out _,
+                out var applied))
+            return;
+        if (!applied.TryGetValue(ent.Comp.Organ, out var markingsSet)) return;
 
-    //     foreach (var marking in markings)
-    //     {
-    //         if (!_starlightMarking.TryGetWaggingId(marking.MarkingId, out _)) continue;
-    //         if (!_actions.GetAction(ent.Comp.ActionEntity).HasValue)
-    //             _actions.AddAction(ent, ref ent.Comp.ActionEntity, ent.Comp.Action);
-    //     }
-    // }
+        foreach (var (_, markings) in markingsSet)
+        foreach (var marking in markings)
+        {
+            if (!_starlightMarking.TryGetWaggingId(marking.MarkingId, out _)) continue;
+            if (!_actions.GetAction(ent.Comp.ActionEntity).HasValue)
+                _actions.AddAction(ent, ref ent.Comp.ActionEntity, ent.Comp.Action);
+        }
+    }
 }
